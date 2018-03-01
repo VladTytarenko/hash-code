0380f57334e71c94a0067c9baa326a20216e6f01
Arseniy <indeoo@ukr.net>
2018-03-01 21:57:24 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/hashcode/App.java

--- src/main/java/com/viva/hashcode/Order.java
+++ src/main/java/com/viva/hashcode/Order.java
@@ -1,15 +1,31 @@
 package com.viva.hashcode;
 
+import static com.viva.hashcode.Order.StatusOrder.WAIT;
+
 public class Order {
     private int minStartTime;
     private int maxFinishTime;
     private Location startLocation;
     private Location finishLocation;
+    StatusOrder statusOrder;
 
     public Order(Location startLocation, Location finishLocation, int minStartTime, int maxFinishTime) {
         this.minStartTime = minStartTime;
         this.maxFinishTime = maxFinishTime;
         this.startLocation = startLocation;
         this.finishLocation = finishLocation;
+        this.statusOrder = WAIT;
+    }
+
+    Location getCurrentTaget() {
+        if (statusOrder == WAIT) {
+            return startLocation;
+        } else {
+            return finishLocation;
+        }
+    }
+
+    enum StatusOrder {
+        WAIT, IN_PROGRES, FINISHED
     }
 }
--- src/main/java/com/viva/hashcode/Vehicle.java
+++ src/main/java/com/viva/hashcode/Vehicle.java
@@ -24,4 +24,24 @@ public class Vehicle {
     public void setOrder(Order order) {
         this.order = order;
     }
+
+    void move() {
+        Location target = order.getCurrentTaget();
+
+        if (currentLocation.getY() != target.getY()) {
+            if (currentLocation.getY() < target.getY()) {
+                currentLocation.setY(currentLocation.getY() + 1);
+            } else {
+                currentLocation.setY(currentLocation.getY() - 1);
+            }
+        } else {
+            if (currentLocation.getX() != target.getX()) {
+                if (currentLocation.getX() < target.getX()) {
+                    currentLocation.setX(currentLocation.getX() + 1);
+                } else {
+                    currentLocation.setX(currentLocation.getX() - 1);
+                }
+            }
+        }
+    }
 }
